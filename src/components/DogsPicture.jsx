e8ca15fa874c61244ce9158d21b71d742273b879
Maycon Cabral <povei@outlook.com>
2022-05-31 20:37:53 -0300

    gallery reponsive

--- src/components/DogsPicture.jsx
+++ src/components/DogsPicture.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect } from 'react';
-// import { PhotoAlbum } from 'react-photo-album';
-import {ImageList, ImageListItem} from '@mui/material';
+import {ImageListItem, imageListItemClasses} from '@mui/material';
+import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Context from '../context/Context';
 
 import { dogImages } from '../API';
@@ -8,10 +9,21 @@ import { dogImages } from '../API';
 function DogsPicture() {
     const { selectBreed } = useContext(Context);
     const [ dogsImg, setDogsImg ] = useState([]);
-    const [ fetchOn, setFetchOn ] = useState(false);
+    const [ fetchOn, setFetchOn ] = useState(false);    
 
     const URL = selectBreed.replace(' ', '/');
 
+    const theme = createTheme({
+        breakpoints: {
+          values: {
+            mobile: 0,
+            bigMobile: 350,
+            tablet: 650,
+            desktop: 900
+          }
+        }
+      });
+    
     useEffect(() => {
         if (URL && fetchOn) {
             dogImages(URL)
@@ -21,27 +33,38 @@ function DogsPicture() {
         };  
         setFetchOn(true)
    }, [URL, fetchOn])
-
-//    const images = dogsImg
-//             .map((imag) => ({ src: imag, width: 200, height:200, alt:`Exemples of ${URL} breed` }))
-//             .slice(0, 15)
-// const images = dogsImg
-//             .map((img) => (img))
-//             .slice(0, 15)
+   
                   
     return (
-      <ImageList sx={{ mt: '1%', width: 'auto', height: 900}} cols={3} rowHeight={'auto'}>
-        {dogsImg.map((item) => (
-          <ImageListItem key={item}>
-            <img
-              src={`${item}?w=164&h=164&fit=crop&auto=format`}
-              srcSet={`${item}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-              alt={`Exemples of ${URL} breed`}
-              loading="lazy"
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
+      <ThemeProvider theme={theme}>
+            <Box sx={{ 
+            gap: 0.5,           
+            overflowY: 'scroll',                             
+            display: "grid",            
+            gridTemplateColumns: {
+            mobile: "repeat(1, 1fr)",
+            bigMobile: "repeat(1, 1fr)",
+            tablet: "repeat(2, 1fr)",
+            desktop: "repeat(3, 1fr)"
+            },
+            [`& .${imageListItemClasses.root}`]: {
+            display: "flex",
+            flexDirection: "column"
+            }
+        }}>
+            {dogsImg.map((item) => (
+            <ImageListItem key={item}>
+                <img
+                src={`${item}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={`Exemples of ${URL} breed`}
+                loading="lazy"
+                />
+            </ImageListItem>
+            ))}
+        </Box>
+      </ThemeProvider>
+      
     );
 };
 
