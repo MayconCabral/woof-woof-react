a42e59740fd4797e0e69614a0f99870ccdd9055e
Maycon Cabral <povei@outlook.com>
2022-09-30 21:33:43 -0300

    [feat] Jest Test

---
+++ src/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {cleanup, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+
+const app = () => render(<App />)
+
+
+describe('Check the elements on the page', () => {    
+  afterEach(() => cleanup())
+
+  test('there is input select', () => {  
+    app() 
+    const input = screen.getByRole('button');
+
+    expect(input).toBeInTheDocument()
+  });
+
+  test('there is a word written "Breed" on the input select', () => {
+    app()
+    const label = screen.getByLabelText('Breed')
+   
+    expect(label).toBeInTheDocument()
+  })
+
+})
